64e31ac4ef121198a1af0e800510e278cd44067f
Garth Lo Bello <garthlobello1@gmail.com>
2025-04-15 23:23:06 -0400

    Update Portfolio_Tracker_app.py

--- Portfolio_Tracker_app.py
+++ Portfolio_Tracker_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
+
 st.set_page_config(
     page_title="Portfolio Tracker Pro+",
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
 import pandas as pd
 import numpy as np
 import base64
